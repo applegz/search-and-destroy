1f603be5e83b5ee63a0b2747c95a80cd4ac2289c
Ellen Granoff <granoff.ellen@gmail.com>
2020-10-28 15:27:03 -0400

    first commit

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,8 +1,27 @@
-'use strict';
+"use strict";
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+  if (array.length === 1) {
+    return array[0] === target;
+  }
+  let midpoint = Math.floor(array.length / 2);
+  if (array[midpoint] === target) {
+    return true;
+  }
+  if (target < array[midpoint]) {
+    return binarySearch(array.slice(0, midpoint), target);
+  } else {
+    return binarySearch(array.slice(midpoint), target);
+  }
+
+  //BASE CASE - when there's one element lef, check if it's the target
+  //assume that the input array is sorted
+  //get the midpoint of the array
+  //compare midpoint to the target
+  //if the target is smaller than the midpoint, call binary search on left half
+  //otherwise call binary search on the right half
+  //if the
 };
 
 /*
@@ -14,4 +33,4 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+module.exports = binarySearch;
