1755f9c33ddb66ec3035c9eeeafa1e15c65b0398
Guoying Zhong <ffamochong@gmail.com>
2020-10-28 16:54:48 -0400

    Looper

--- looper/isloop.js
+++ looper/isloop.js
@@ -2,9 +2,35 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+  // console.log(linkedlist);
 
+  // this is to determine if we are in linkedlist OR node
+  let current;
+  if (linkedlist.head) {
+    current = linkedlist.head;
+  } else {
+    current = linkedlist;
+  }
+  //--------------------
+  console.log(`current.value`, current.value);
+
+  let nextOfCurrent = current.next;
+  console.log(`nextOfCurrent`, nextOfCurrent);
+  let previous = current.next.previous;
+  console.log(`previous.value`, previous.value);
+  console.log(`nextOfCurrent.value`, nextOfCurrent.value);
+  // console.log(previous.value === next.value);
+  console.log(`previous`, previous);
+
+  if (!next) {
+    return false;
+  }
+  if (next.value !== previous.value) {
+    return true;
+  }
+  console.log(`we are in the loop now.`);
+  isLoop(next);
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +40,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
